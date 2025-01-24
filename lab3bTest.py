e32f8a396a9abe1aeb03338fa1ad38f92dd4fbd0
Sangeeth Vethanayagam <svethanayagam3@myseneca.ca>
2025-01-23 23:56:42 -0500

    added lab3b and lab3btest

---
+++ lab3bTest.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import lab3b
+
+lab3b.sum_numbers(10, 5)
+# Will return 15
+lab3b.sum_numbers(25, 25)
+# Will return 50
+lab3b.subtract_numbers(10, 5)
+# Will return 5
+lab3b.subtract_numbers(5, 10)
+# Will return -5
+lab3b.multiply_numbers(10, 5)
+# Will return 50
+lab3b.multiply_numbers(10, 2)
+# Will return 20
