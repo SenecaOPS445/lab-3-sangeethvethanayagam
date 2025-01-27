246c86d4b9765fa240cbdbf4f8d170d1b01d1a9c
Sangeeth Vethanayagam <svethanayagam3@myseneca.ca>
2025-01-26 19:26:02 -0500

    Update lab3d.py

--- lab3d.py
+++ lab3d.py
@@ -1,3 +1,15 @@
 #!/usr/bin/env python3
 
 # Author ID: 189353238
+
+import subprocess
+
+
+
+def free_space():
+    p = subprocess.Popen("df -h | grep '/$' | awk '{print $4}' ")
+    output = p.communicate()
+    stdout = output[0].decode('utf-8').strip()
+    return stdout
+
+free_space()
